71114e2e94c0e6d719f95c546fd1148ced639114
aneeq-ahmed12 <144549605+aneeq-ahmed12@users.noreply.github.com>
2023-09-16 19:54:09 +0000

    conversion operation

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,14 @@
+
+
+/*
+let numberOne = 43;
+let convertIntoString = String(numberOne);
+console.log(typeof(convertIntoString));
+console.log(convertIntoString);
+*/
+
+
+let isLoggedIn = 1;
+let convertIntoBoolean = Boolean(isLoggedIn);
+console.log(convertIntoBoolean);
+console.log(typeof(convertIntoBoolean));
