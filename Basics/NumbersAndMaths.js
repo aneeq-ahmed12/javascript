a441b6049fe2302f7a353a21565654cb7bcd4e4d
aneeq-ahmed12 <144549605+aneeq-ahmed12@users.noreply.github.com>
2023-09-24 19:07:51 +0000

    numbers and maths

---
+++ Basics/NumbersAndMaths.js
@@ -0,0 +1,24 @@
+const balance = new Number(345);
+//console.log(balance.toFixed(2));
+
+
+const number = 234.7689;
+//console.log(number.toPrecision(4));
+
+
+const value = 1000000000;
+//console.log(value.toLocaleString());
+
+//console.log(Math.abs(-6));
+
+// console.log(Math.round(3.5));
+// console.log(Math.round(4.4));
+// console.log(Math.round(5.7));
+
+// console.log(Math.ceil(3.3));
+// console.log(Math.floor(4.7));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+
