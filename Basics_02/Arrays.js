54aee656213e4b1d312e0ca0b2dcba0a7ac0517e
aneeq-ahmed12 <144549605+aneeq-ahmed12@users.noreply.github.com>
2023-09-26 21:31:41 +0000

    arrays

---
+++ Basics_02/Arrays.js
@@ -0,0 +1,22 @@
+const cities = [1,2,3,4,5,6,7];
+
+//console.log(cities[1]);
+
+
+const myArr = new Array(1,2,3,4,5,6);
+//console.log(typeof(myArr));
+
+//console.log(cities.slice(1,2));
+
+
+//console.log(cities.splice(1,4));
+
+//console.log(cities);
+
+
+
+
+
+
+const one_array = [1,2,4,[5,2,4],[3,[4,5,3]]];
+console.log(one_array.flat(Infinity));
