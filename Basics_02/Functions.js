2d72db742b684b581f9f94451ad40bd6bc81a2b7
aneeq-ahmed12 <144549605+aneeq-ahmed12@users.noreply.github.com>
2023-10-07 18:19:20 +0000

    Functions

---
+++ Basics_02/Functions.js
@@ -0,0 +1,49 @@
+
+/*
+function AddingTwoNumbers(num1,num2)
+{
+    
+    return num1 + num2;
+
+}
+
+console.log(AddingTwoNumbers(3,7));
+*/
+
+/*
+const Customer = {
+    FullName : "Aneeq Ahmed",
+    City : "Orebro",
+    Country : "Sweden"
+}
+
+function HandleObjects(objectName){
+    
+    console.log(`Name of customer is ${objectName.FullName} and city name is ${objectName.City}`);
+} 
+
+Two methods to call function 
+
+Method#1
+HandleObjects(Customer);
+
+Method#2
+HandleObjects({
+    FullName : "Aneeq Ahmed",
+    City : "Orebro",
+    Country : "Sweden"
+});
+*/
+
+const NameOfAirlines = ["Turkish", "Emirates", "Qatar", "Wizzair", "Ryanair"];
+
+
+function HandleArrays(getArray)
+{
+    console.log(getArray[3]);
+}
+
+HandleArrays(NameOfAirlines);
+
+
+
