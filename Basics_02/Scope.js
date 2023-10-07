3c2e13b8c367d66f426311bcdb9ab6b895211afc
aneeq-ahmed12 <144549605+aneeq-ahmed12@users.noreply.github.com>
2023-10-07 19:28:25 +0000

    Scope in javascrit

---
+++ Basics_02/Scope.js
@@ -0,0 +1,14 @@
+
+function One()
+{
+    let nameOne = "Aneeq";
+    function Two()
+    {
+        let nameTwo = "Adeel";
+        console.log(`Name is ${nameOne}`);
+    }
+    console.log("Hello");
+    Two();
+}
+
+One();
