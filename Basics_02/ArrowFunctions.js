e0eb29e4107e834b2ace54ad14127f280fbc7b2b
aneeq-ahmed12 <144549605+aneeq-ahmed12@users.noreply.github.com>
2023-10-08 18:27:47 +0000

    Arrow function

---
+++ Basics_02/ArrowFunctions.js
@@ -0,0 +1,14 @@
+//Arrow Function
+
+const multiply = (num1,num2) => {
+    return num1 * num2;
+}
+
+console.log(multiply(7,8));
+
+
+//Implicit Return
+
+const addition = (num1,num2) => (num1 + num2);
+
+console.log(addition(7,8));
